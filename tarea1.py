081b20a2674e481884198f87215997537bd0904b
bry4ann02 <98840838+bry4ann02@users.noreply.github.com>
2023-10-05 21:00:25 -0600

    Delete tarea1.py

--- tarea1.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd #importamos pandas 
-url = "https://raw.githubusercontent.com/erikgregorywebb/data/main/data/spotify-charts-daily-2020-10-07.csv" #escribimos la url de nuestro csv
-
-
-df = pd.read_csv(url) #creamos un dataframe con pandas para ver nuestro url
-df.to_csv('spotify-charts-daily-2020-10-07.csv', index =False) #lo descargamos en forma de df con index falsos para que no tenga formato y listo
